70ae68873b0c18e538832902802356eea5815abb
Joe Boxberger <jboxberger@sandiego.edu>
2024-12-03 15:47:15 -0800

    Created test pages for image upload and viewing history

--- port_inspector/port_inspector_app/models.py
+++ port_inspector/port_inspector_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class ImageSet(models.Model):
+    upload_id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to='')
--- port_inspector/port_inspector_app/views.py
+++ port_inspector/port_inspector_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.template import loader
+from django.conf import settings
+from . import forms
+from .models import ImageSet
 
 # Create your views here.
+def upload_image(request):
+    # if the user is attempting to POST, aka submitting the form
+    if request.method == "POST":
+        #form filled with the request information
+        image_form = forms.ImageForm(request.POST, request.FILES)
+        if image_form.is_valid():
+            new_image_set = image_form.save(commit=False)
+            new_image_set.save()
+            #potentially redirect to a new page here
+    #else it is a GET request, meaning the user is requesting the page, in which we should give them an empty form
+    else:
+        image_form = forms.ImageForm()  
+    return render(request, 'upload_photo.html', {'form' : image_form})
+
+
+def view_history(request):
+    image_sets = ImageSet.objects.all()
+    return render(request, 'history.html', {'images' : image_sets, 'MEDIA_URL' : settings.MEDIA_URL})
