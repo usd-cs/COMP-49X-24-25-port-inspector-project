7bf2aefc4aa70fc18fa125a3f3ca9a64624d9f58
Joe Boxberger <jboxberger@sandiego.edu>
2024-12-04 17:42:59 -0800

    Fix linting issues

--- port_inspector/port_inspector_app/views.py
+++ port_inspector/port_inspector_app/views.py
@@ -8,23 +8,23 @@ from port_inspector_app.models import Image, SpecimenUpload
 def upload_image(request):
     # if the user is attempting to POST, aka submitting the form
     if request.method == "POST":
-        #form filled with the request information
+        # form filled with the request information
         image_form = forms.ImageForm(request.POST, request.FILES)
         if image_form.is_valid():
-            #generate a new specimen upload
+            # generate a new specimen upload
             specimen_upload = SpecimenUpload()
-            #TODO implement user session
-            specimen_upload.user = None #placeholder, set user to null for now
+            # TODO implement user session
+            specimen_upload.user = None # placeholder, set user to null for now
 
             new_image = image_form.save(commit=False)
             new_image.specimen_upload = specimen_upload
 
             specimen_upload.save()
             new_image.save()
-            #potentially redirect to a new page here
-    #else it is a GET request, meaning the user is requesting the page, in which we should give them an empty form
+            # potentially redirect to a new page here
+    # else it is a GET request, meaning the user is requesting the page, in which we should give them an empty form
     else:
-        image_form = forms.ImageForm()  
+        image_form = forms.ImageForm()
     return render(request, 'upload_photo.html', {'form' : image_form})
 
 
