28392f22d0b5476aa3fb7104e4c15eceba97363c
AudreyKrishnadasan <98282129+AudreyKrishnadasan@users.noreply.github.com>
2025-02-18 16:31:12 -0800

    fix: odd flake error showing up on github but not my computer

--- port_inspector/port_inspector_app/views.py
+++ port_inspector/port_inspector_app/views.py
@@ -51,7 +51,7 @@ def verify_email_confirm(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):  # noqa: E275
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_email_verified = True
