d8f1b393b60f70a9f9f5621250dd8758cc1c13ce
Sara Evans <saraevans@sandiego.edu>
2025-02-27 15:29:11 -0800

    Implemented url for results page

--- port_inspector/port_inspector/urls.py
+++ port_inspector/port_inspector/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('verify-email/done/', views.verify_email_done, name='verify-email-done'),
     path('verify-email-confirm/<uidb64>/<token>/', views.verify_email_confirm, name='verify-email-confirm'),
     path('verify-email/complete/', views.verify_email_complete, name='verify-email-complete'),
+    path('results/', views.results_view, name='results'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # storing uploaded images to our MEDIA_URL
